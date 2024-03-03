8787abf1aa1a94ed19cc6c5f860285339520b1b4
SauvikRay <sauvikray001@gmail.com>
2024-03-03 19:24:15 +0600

    Change notifier added

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,28 +1,63 @@
-import 'package:get/get.dart';
+import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'category_model.dart';
 import 'subcategory_model.dart';
 
-class DynamicController extends GetxController{
+class DynamicController extends ChangeNotifier {
+  List<Category> category = <Category>[];
+  List<SubCategory> subcategory = <SubCategory>[];
+  List<SubCategory> allSubcategory = <SubCategory>[];
+  Map<String, SubCategory> selectedChips = <String, SubCategory>{};
+  Map<String, ChildCategory> selectedChildChips = <String, ChildCategory>{};
+  List<ChildCategory> childCategory = <ChildCategory>[];
+  List<ChildCategory> allChildCategoryItem = <ChildCategory>[];
+  bool isVisible = false;
 
-   RxList<Category> category=<Category>[].obs;
-  RxList<SubCategory> subcategory=<SubCategory>[].obs;
-  RxList<SubCategory> allSubcategory=<SubCategory>[].obs;
-RxMap<String ,SubCategory> selectedChips=<String ,SubCategory>{}.obs;
-RxMap<String ,ChildCategory> selectedChildChips=<String ,ChildCategory>{}.obs;
-RxList<ChildCategory> childCategory=<ChildCategory>[].obs;
-List<ChildCategory> allChildCategoryItem=<ChildCategory>[].obs;
-
-
-
-  parseSubcategory(){
-      CategoryResponse categoryResponse= CategoryResponse.fromJson(categoryResponseData);
-      category.value=categoryResponse.data??[];
-      SubcategoryResponse subcategoryResponse= SubcategoryResponse.fromJson(subcategoryResponseData);
-      SubcategoryResponse subcategoryResponse2= SubcategoryResponse.fromJson(subcategoryResponseData);
-      allSubcategory.value= subcategoryResponse2.data??[];
-      subcategory.value= subcategoryResponse.data??[];
-
+  parseSubcategory() {
+    CategoryResponse categoryResponse = CategoryResponse.fromJson(categoryResponseData);
+    category = categoryResponse.data ?? [];
+    notifyListeners();
+    SubcategoryResponse subcategoryResponse = SubcategoryResponse.fromJson(subcategoryResponseData);
+    SubcategoryResponse subcategoryResponse2 = SubcategoryResponse.fromJson(subcategoryResponseData);
+    allSubcategory = subcategoryResponse2.data ?? [];
+    notifyListeners();
+    subcategory = subcategoryResponse.data ?? [];
+    notifyListeners();
   }
 
+  ifSelectedSubCategory({required bool value, required SubCategory element}) {
+    isVisible = value;
+    // log('allSubcategory length: ${allSubcategory.length}');
+    selectedChips[element.subCateId ?? ''] = element;
+    subcategory.removeWhere((item) => item.subCateId != element.subCateId);
+    childCategory.addAll(element.child ?? []);
+    allChildCategoryItem.clear();
+    allChildCategoryItem.addAll(childCategory);
+    notifyListeners();
+  }
+
+  elseSelectedSubCategory({required bool value, required SubCategory element}) {
+    isVisible = value;
+    selectedChips.remove(element.subCateId);
+    subcategory.remove(element);
+
+    // log('allSubcategory length: ${allSubcategory.length}');
+    Set<SubCategory> uniqueItem = Set<SubCategory>.from(allSubcategory).union(Set<SubCategory>.from(subcategory));
+    // log('Item length: ${uniqueItem.length}');
+
+    subcategory.addAll(uniqueItem.toList());
+
+    childCategory.clear();
+    if (selectedChildChips.isNotEmpty) {
+      selectedChildChips.clear();
+    }
+
+    log('Selected Child :${selectedChildChips.length}');
+
+    allChildCategoryItem.clear();
+    allChildCategoryItem.addAll(childCategory);
+
+    notifyListeners();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:get/get.dart';
 import 'package:ui_kit/controller.dart';
-
-import 'category_model.dart';
 import 'subcategory_model.dart';
 
 void main() {
@@ -41,8 +38,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  RxBool isVisible = false.obs;
-  final _controller = Get.put(DynamicController());
+  final _controller = DynamicController();
   @override
   void initState() {
     _controller.parseSubcategory();
@@ -72,148 +68,125 @@ class _MyHomePageState extends State<MyHomePage> {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  Obx(
-                    () => ListView.separated(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: _controller.subcategory.length,
-                        separatorBuilder: (context, index) {
-                          return const SizedBox(width: 10);
-                        },
-                        itemBuilder: (context, index) {
-                          SubCategory element = _controller.subcategory[index];
-                          final isSelected = _controller.selectedChips.containsKey(element.subCateId);
-                          return Animate(
-                            effects: const [
-                              SlideEffect(begin: Offset(1, 0), end: Offset(0, 0), duration: Duration(milliseconds: 600), curve: Curves.easeOutSine),
-                              FadeEffect(begin: 0.0, end: 1.0, duration: Duration(milliseconds: 300), curve: Curves.easeOutSine)
-                            ],
-                            child: ChoiceChip(
-                              avatar: isSelected
-                                  ? const Icon(
-                                      CupertinoIcons.clear,
-                                      size: 15,
-                                      color: Colors.white,
-                                    )
-                                  : null,
-                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              labelPadding: EdgeInsets.zero,
-                              backgroundColor: Colors.transparent,
-                              selectedColor: CupertinoColors.systemBlue,
-                              labelStyle: TextStyle(fontSize: 14, color: isSelected ? Colors.white : CupertinoColors.activeBlue, fontWeight: isSelected ? FontWeight.bold : FontWeight.normal),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: const BorderRadius.all(Radius.circular(5)), side: BorderSide(color: isSelected ? CupertinoColors.activeBlue : CupertinoColors.inactiveGray)),
-                              label: Text("${element.subCateName}"),
-                              selected: isSelected,
-                              onSelected: (value) {
-                                // log('Selected Chips value $value');
-                                if (value) {
-                                  isVisible.value = value;
-                                  // log('allSubcategory length: ${allSubcategory.length}');
-                                  _controller.selectedChips[element.subCateId ?? ''] = element;
-                                  _controller.subcategory.removeWhere((item) => item.subCateId != element.subCateId);
+                  ListView.separated(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _controller.subcategory.length,
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(width: 10);
+                      },
+                      itemBuilder: (context, index) {
+                        SubCategory element = _controller.subcategory[index];
+                        final isSelected = _controller.selectedChips.containsKey(element.subCateId);
+                        return Animate(
+                          effects: const [
+                            SlideEffect(begin: Offset(1, 0), end: Offset(0, 0), duration: Duration(milliseconds: 600), curve: Curves.easeOutSine),
+                            FadeEffect(begin: 0.0, end: 1.0, duration: Duration(milliseconds: 300), curve: Curves.easeOutSine)
+                          ],
+                          child: ChoiceChip(
+                            avatar: isSelected
+                                ? const Icon(
+                                    CupertinoIcons.clear,
+                                    size: 15,
+                                    color: Colors.white,
+                                  )
+                                : null,
+                            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            labelPadding: EdgeInsets.zero,
+                            backgroundColor: Colors.transparent,
+                            selectedColor: CupertinoColors.systemBlue,
+                            labelStyle: TextStyle(fontSize: 14, color: isSelected ? Colors.white : CupertinoColors.activeBlue, fontWeight: isSelected ? FontWeight.bold : FontWeight.normal),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: const BorderRadius.all(Radius.circular(5)), side: BorderSide(color: isSelected ? CupertinoColors.activeBlue : CupertinoColors.inactiveGray)),
+                            label: Text("${element.subCateName}"),
+                            selected: isSelected,
+                            onSelected: (value) {
+                              // log('Selected Chips value $value');
+                              if (value) {
+                                // isVisible = value;
+                                // // log('allSubcategory length: ${allSubcategory.length}');
+                                // _controller.selectedChips[element.subCateId ?? ''] = element;
+                                // _controller.subcategory.removeWhere((item) => item.subCateId != element.subCateId);
 
-                                  // _controller.allChildCategoryItem=element.child??[];
-                                  // _controller.childCategory.value=element.child??[];
-                                  _controller.childCategory.addAll(element.child ?? []);
+                                // // _controller.allChildCategoryItem=element.child??[];
+                                // // _controller.childCategory.value=element.child??[];
+                                // _controller.childCategory.addAll(element.child ?? []);
+                                _controller.ifSelectedSubCategory(value: value, element: element);
 
-                                  // setState(() {});
-                                } else {
-                                  isVisible.value = value;
-                                  _controller.selectedChips.remove(element.subCateId);
-                                  _controller.subcategory.remove(element);
-
-                                  // log('allSubcategory length: ${allSubcategory.length}');
-                                  Set<SubCategory> uniqueItem = Set<SubCategory>.from(_controller.allSubcategory).union(Set<SubCategory>.from(_controller.subcategory));
-                                  // log('Item length: ${uniqueItem.length}');
-
-                                  _controller.subcategory.addAll(uniqueItem.toList());
-
-                                  _controller.childCategory.clear();
-                                  if (_controller.selectedChildChips.isNotEmpty) {
-                                    _controller.selectedChildChips.clear();
-                                  }
-
-                                  log('Selected Child :${_controller.selectedChildChips.length}');
-                                  // setState(() {});
-                                }
-                                // _controller.allChildCategoryItem.removeWhere((element) => element.id==_controller.childCategory.);
-                                _controller.allChildCategoryItem.assignAll(_controller.childCategory);
-
-                                //  selectedSubCategory.value = subcategory.elementAt(index);
-                              },
-                            ),
-                          );
-                        }),
-                  ),
+                                setState(() {});
+                              } else {
+                                _controller.elseSelectedSubCategory(value: value, element: element);
+                                setState(() {});
+                              }
+                              //  selectedSubCategory.value = subcategory.elementAt(index);
+                            },
+                          ),
+                        );
+                      }),
                   const SizedBox(
                     width: 10,
                   ),
-                  Obx(
-                    () => Visibility(
-                      visible: isVisible.value,
-                      child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: _controller.childCategory.length,
-                        separatorBuilder: (context, index) => const SizedBox(width: 10),
-                        itemBuilder: (context, index) {
-                          ChildCategory item = _controller.childCategory[index];
-                          final isChildSelected = _controller.selectedChildChips.containsKey(item.id);
-                          return Animate(
-                            effects: const [
-                              SlideEffect(begin: Offset(1, 0), end: Offset(0, 0), duration: Duration(milliseconds: 600), curve: Curves.easeOutSine),
-                              FadeEffect(begin: 0.0, end: 1.0, duration: Duration(milliseconds: 300), curve: Curves.easeOutSine)
-                            ],
-                            child: ChoiceChip(
-                              avatar: isChildSelected
-                                  ? const Icon(
-                                      CupertinoIcons.clear,
-                                      size: 15,
-                                      color: Colors.white,
-                                    )
-                                  : null,
-                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              labelPadding: EdgeInsets.zero,
-                              backgroundColor: Colors.transparent,
-                              selectedColor: CupertinoColors.systemBlue,
-                              labelStyle:
-                                  TextStyle(fontSize: 14, color: isChildSelected ? Colors.white : CupertinoColors.activeBlue, fontWeight: isChildSelected ? FontWeight.bold : FontWeight.normal),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: const BorderRadius.all(Radius.circular(5)), side: BorderSide(color: isChildSelected ? CupertinoColors.activeBlue : CupertinoColors.inactiveGray)),
-                              label: Text(item.name ?? ''),
-                              selected: isChildSelected,
-                              onSelected: (value) {
-                                log('if length:${_controller.allChildCategoryItem.length}');
-                                if (value) {
-                                  // log('if condition length:${allChildCategoryItem.length}');
-                                  _controller.selectedChildChips[item.id ?? ''] = item;
-                                  _controller.childCategory.removeWhere((element) => element.id != item.id);
-                                  // setState(() {});
-                                } else {
-                                  _controller.selectedChildChips.remove(item.id);
-                                  _controller.selectedChildChips.clear();
-                                  _controller.childCategory.remove(item);
-                                  Set<ChildCategory> uniqueItem = Set<ChildCategory>.from(_controller.allChildCategoryItem).union(Set<ChildCategory>.from(_controller.childCategory));
-                                  // log('Child Item length: ${uniqueItem.length}');
+                  Visibility(
+                    visible: _controller.isVisible,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: _controller.childCategory.length,
+                      separatorBuilder: (context, index) => const SizedBox(width: 10),
+                      itemBuilder: (context, index) {
+                        ChildCategory item = _controller.childCategory[index];
+                        final isChildSelected = _controller.selectedChildChips.containsKey(item.id);
+                        return Animate(
+                          effects: const [
+                            SlideEffect(begin: Offset(1, 0), end: Offset(0, 0), duration: Duration(milliseconds: 600), curve: Curves.easeOutSine),
+                            FadeEffect(begin: 0.0, end: 1.0, duration: Duration(milliseconds: 300), curve: Curves.easeOutSine)
+                          ],
+                          child: ChoiceChip(
+                            avatar: isChildSelected
+                                ? const Icon(
+                                    CupertinoIcons.clear,
+                                    size: 15,
+                                    color: Colors.white,
+                                  )
+                                : null,
+                            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            labelPadding: EdgeInsets.zero,
+                            backgroundColor: Colors.transparent,
+                            selectedColor: CupertinoColors.systemBlue,
+                            labelStyle: TextStyle(fontSize: 14, color: isChildSelected ? Colors.white : CupertinoColors.activeBlue, fontWeight: isChildSelected ? FontWeight.bold : FontWeight.normal),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: const BorderRadius.all(Radius.circular(5)), side: BorderSide(color: isChildSelected ? CupertinoColors.activeBlue : CupertinoColors.inactiveGray)),
+                            label: Text(item.name ?? ''),
+                            selected: isChildSelected,
+                            onSelected: (value) {
+                              log('if length:${_controller.allChildCategoryItem.length}');
+                              if (value) {
+                                // log('if condition length:${allChildCategoryItem.length}');
+                                _controller.selectedChildChips[item.id ?? ''] = item;
+                                _controller.childCategory.removeWhere((element) => element.id != item.id);
+                                setState(() {});
+                              } else {
+                                _controller.selectedChildChips.remove(item.id);
+                                _controller.selectedChildChips.clear();
+                                _controller.childCategory.remove(item);
+                                Set<ChildCategory> uniqueItem = Set<ChildCategory>.from(_controller.allChildCategoryItem).union(Set<ChildCategory>.from(_controller.childCategory));
+                                // log('Child Item length: ${uniqueItem.length}');
 
-                                  _controller.childCategory.addAll(uniqueItem.toList());
+                                _controller.childCategory.addAll(uniqueItem.toList());
 
-                                  // setState(() {});
+                                log('sdfsdd : ${_controller.allChildCategoryItem.length}');
+                                setState(() {});
 
-                                  log('sdfsdd : ${_controller.allChildCategoryItem.length}');
-
-                                  // for(var iiii in allChildCategoryItem){
-                                  //   log('All child Item: ${iiii.name}');
-                                  // }
-                                }
-                              },
-                            ),
-                          );
-                        },
-                      ),
+                                // for(var iiii in allChildCategoryItem){
+                                //   log('All child Item: ${iiii.name}');
+                                // }
+                              }
+                            },
+                          ),
+                        );
+                      },
                     ),
                   ),
                 ],
